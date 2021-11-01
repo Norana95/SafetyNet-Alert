0e99abc7ac38cb372c824bdc4a7593b68910dfd1
Norana95 <boublenza.n@gmail.com>
2021-11-01 21:54:31 +0100

    Initial commit

---
+++ src/test/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SafetyNet.SafetyNetAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
